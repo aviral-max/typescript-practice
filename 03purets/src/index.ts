101f5ed010372a6b5a00e486d55278b7fe34a719
aviral-max <aviral.kaushal@maxhealthcare.com>
2025-12-10 16:27:03 +0530

    completed with getters and setters functions in ts

--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -12,8 +12,30 @@
 // }
 
 class User {
+  private _courseCount = 1;
   readonly city: string = "New Delhi";
   constructor(public email: string, public name: string) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  //getters and setters in ts
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  //also, you cannot set return type of setter functions in ts
+  set courseCount(courseNum: number) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const aviral = new User("aviral@example.com", "Aviral");
