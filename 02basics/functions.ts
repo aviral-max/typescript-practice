498be0bbf886e1a1d7cf7a2fe68bbdf8ee437ec4
aviral-max <aviral.kaushal@maxhealthcare.com>
2025-12-10 12:47:19 +0530

    function return types and diff between void and never

--- 02basics/functions.ts
+++ 02basics/functions.ts
@@ -17,4 +17,28 @@ getUpper("aviral");
 signUpUser("aviral", "aviral@example.com", true);
 loginUser("a", "a@a.com");
 
+// function getValue(myVal: number) {
+//     if(myVal > 5) {
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getHello = (s: string): string => {
+  return "";
+};
+
+const heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+heros.map((hero) => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
 export {};
