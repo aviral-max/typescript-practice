76460c64df992b4fcdbf32a5d4a99d8c09aba571
aviral-max <aviral.kaushal@maxhealthcare.com>
2025-12-11 11:31:17 +0530

    type predicate and instanceof narrowing

--- 03purets/src/typeNarrowing_detection.ts
+++ 03purets/src/typeNarrowing_detection.ts
@@ -44,3 +44,38 @@ function isAdminAccount(account: User | Admin) {
     return account.isAdmin;
   }
 }
+
+//instance of class Date (instanceof narrowing)
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toUpperCase());
+  }
+}
+
+// 👉 Type Predicate (User-defined type guard)
+type Fish = {
+  swim: () => void;
+};
+
+type Bird = {
+  fly: () => void;
+};
+// This function tells TypeScript:
+// “If isFish(pet) returns true, then pet is guaranteed to be a Fish.”
+// It helps TypeScript narrow the type from Fish | Bird into Fish inside the if-block.
+function isFish(pet: Fish | Bird): pet is Fish {
+  // type predicate
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet; // ts is still confused whether pet is Fish or Bird here but it should be Fish
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
+  }
+}
